b362ce524900c52a14ad2375b77fe411c36a3696
Erik Henning Thiede <thiede@uchicago.edu>
2016-02-18 22:26:54 -0600

    Small Bugfixes, Added Demo, Organized.

--- test.sh
+++
@@ -1,3 +0,0 @@
-module load python
-#python /project/weare-dinner/ethiede/EMUS/emus.py whammeta.txt -180.0 180.0 100 -180.0 180.0 100 --neighbor 2.5 --period 360 360 
-python /project/weare-dinner/ethiede/EMUS/emus.py Testcase/1dwhammeta.txt -180.0 180.0 100 --period 360 --zasymptoticvar 5 13
