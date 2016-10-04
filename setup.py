bd728562df882d647c162e0310380d6178c4fc32
Erik Henning Thiede <thiede@uchicago.edu>
2016-10-04 11:23:34 -0500

    Updated documentation.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup,find_packages
+from setuptools import setup,find_packages
 
 setup(name='EMUS',
     version='0.1.0',
@@ -11,7 +11,7 @@ setup(name='EMUS',
         'Development Status :: 2 - Pre-Alpha',
         'Topic :: Scientific/Engineering'
         ],
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=['numpy','hdf5','acor'], # Does this do what you think, Erik?
 
     )
