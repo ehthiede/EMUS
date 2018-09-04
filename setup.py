cb3a28977fee975f0d5a63aae8faed7845d8b051
Erik Henning Thiede <ehthiede@gmail.com>
2018-09-04 11:23:58 -0500

    Tweaked Setup Script

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(name='emus',
-    version='0.9.2b1',
-    description="Tools and Methods associated with the Eigenvector Method for Umbrella Sampling (EMUS)",
-    license='GPL',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 2.7',
-        ],
-    packages=['emus'],
-    scripts=['scripts/wemus.py'],
-    install_requires=['numpy','scipy','h5py','acor'],
-    url='https://github.com/ehthiede/EMUS',
-    author='Erik Henning Thiede'
+      version='0.9.3b',
+      description="Tools and Methods associated with the Eigenvector Method for Umbrella Sampling (EMUS)",
+      license='GPL',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Topic :: Scientific/Engineering',
+          'Programming Language :: Python :: 2.7',
+      ],
+      packages=['emus'],
+      scripts=['scripts/wemus.py'],
+      install_requires=['numpy', 'scipy', 'h5py', 'acor'],
+      url='https://github.com/ehthiede/EMUS',
+      author='Erik Henning Thiede'
 
-    )
+      )
