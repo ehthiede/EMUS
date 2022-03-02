77dec3bac708bd4d2dd61d8a547ce7f1025f121a
Erik Henning Thiede <ehthiede@gmail.com>
2022-03-02 16:36:16 -0500

    Updated iterative demo

--- examples/AlaDipeptide_1D/demo_iter.py
+++ examples/AlaDipeptide_1D/demo_iter.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Example script with basic usage of the EMUS package.
-The script follows the quickstart guide closely, with slight adjustments (for simplicity we have moved all plotting commands to the bottom of the script).
+Example script with basic usage of the EMUS package for MBAR estimates
+using the iterative EMUS solver.
+(for simplicity we have moved all plotting commands to the bottom of the script).
 Please note that the demo requires matplotlib, which is not a dependency in the emus package to keep things lightweight.
 """
 import numpy as np
 from emus import usutils as uu
-from emus import emus, avar, iter_avar
+from emus import emus, iter_avar
 import matplotlib.pyplot as plt
 
 # Define Simulation Parameters
@@ -23,106 +24,48 @@ psis, cv_trajs, neighbors = uu.data_from_meta(
     meta_file, dim, T=T, k_B=k_B, period=period)
 
 # Calculate the partition function for each window
-z, F = emus.calculate_zs(psis, neighbors=neighbors)
 z_iter, F_iter = emus.calculate_zs(psis, neighbors=neighbors, n_iter=5)
 
-# Calculate error in each z value from the first iteration.
-zerr, zcontribs, ztaus = avar.calc_partition_functions(
-    psis, z, F, iat_method='acor')
-# psis, z, F, iat_method=np.ones(len(psis)))
-log_zerr = np.sqrt(zerr) / z
-
-# log_zerr_iter, log_zcontribs_iter, log_ztaus_iter = iter_avar.calc_log_z(
-#     psis, z_iter, iat_method='acor')
-log_zerr_iter, log_zcontribs_iter, log_ztaus_iter = iter_avar.calc_log_z(
-    # psis, z_iter, iat_method = np.ones(len(psis)))
-    psis, z_iter, iat_method=1, repexchange=True)
-print(log_zerr_iter)
-
-N = len(z)
-plt.errorbar(np.arange(N), -np.log(z), yerr=log_zerr, label='noniterative')
-plt.errorbar(np.arange(N), -np.log(z_iter), yerr=np.sqrt(log_zerr_iter), label='iterative')
+plt.plot(np.arange(len(z_iter)), -np.log(z_iter), label='Window Free Energies')
+plt.xlabel(r'Window Index')
+plt.ylabel('Window FE Unitless')
 plt.legend()
 plt.show()
 
-plt.plot(np.arange(N), log_zerr, label='noniterative')
-plt.plot(np.arange(N), np.sqrt(log_zerr_iter), label='iterative')
-plt.legend()
-plt.show()
-
-raise Exception
-
-# # Calculate error in each z value from the first iteration.
-# iter_avar.calc_log_z(
-#     psis, z, iat_method='acor', repexchange=True)
-# raise Exception()
-
-# Calculate the PMF from EMUS
 domain = ((-180.0, 180.))            # Range of dihedral angle values
-pmf, edges = emus.calculate_pmf(
-    cv_trajs, psis, domain, z, nbins=nbins, kT=kT, use_iter=False)   # Calculate the pmf
-
-# Calculate z using the MBAR iteration.
-z_iter_1, F_iter_1 = emus.calculate_zs(psis, n_iter=1)
-z_iter_2, F_iter_2 = emus.calculate_zs(psis, n_iter=2)
-z_iter_5, F_iter_5 = emus.calculate_zs(psis, n_iter=5)
-z_iter_1k, F_iter_1k = emus.calculate_zs(psis, n_iter=1000)
-
 # Calculate new PMF
 iterpmf, edges = emus.calculate_pmf(
-    cv_trajs, psis, domain, nbins=nbins, z=z_iter_1k, kT=kT)
+    cv_trajs, psis, domain, nbins=nbins, z=z_iter, kT=kT)
 
-# Estimate probability of being in C7 ax basin
 fdata = [((traj > 25) & (traj < 100)).flatten() for traj in cv_trajs]
-# Calculate the probability and perform error analysis.
-iat, probC7ax, probC7ax_contribs = avar.calc_avg_ratio(
-    psis, z, F, fdata, iat_method='acor')
-
-probC7ax_std = np.sqrt(np.sum(probC7ax_contribs))
-# This command just calculates the probability, without error analysis.
 prob_C7ax_iter = emus.calculate_obs(
-    psis, z_iter_1k, fdata, use_iter=True)  # Just calculate the probability
+    psis, z_iter, fdata, use_iter=True)  # Just calculate the probability
+
+fe_C7ax = -np.log(prob_C7ax_iter)
+fe_C7ax_err, fe_C7ax_contribs, fe_C7ax_taus = iter_avar.calc_log_avg_ratio(psis, z_iter, fdata, neighbors=neighbors)
+
 avg_pmf, edges = emus.calculate_avg_on_pmf(
-    cv_trajs, psis, (-180, 180), z_iter_1k, fdata, use_iter=True, nbins=nbins)  # Just calculate the probability
-# plt.plot(avg_pmf)
-# plt.xlabel('Dihedral Angle')
-# plt.ylabel('Avg of Indicator on PMF')
-# plt.show()
-
-
-# Get the asymptotic error of each histogram bin.
-pmf_av_mns, pmf_avars = avar.calc_pmf(
-    cv_trajs, psis, domain, z, F, nbins=nbins, kT=kT, iat_method=np.average(ztaus, axis=0))
+    cv_trajs, psis, (-180, 180), z_iter, fdata, use_iter=True, nbins=nbins)
 
 
 # ~~~ Data Output Section ~~~ #
-
 # Plot the EMUS, Iterative EMUS pmfs.
 pmf_centers = (edges[0][1:]+edges[0][:-1])/2.
 plt.figure()
-plt.errorbar(pmf_centers, pmf_av_mns, yerr=np.sqrt(
-    pmf_avars), label='EMUS PMF w. AVAR')
 plt.plot(pmf_centers, iterpmf, label='Iter EMUS PMF')
 plt.xlabel(r'$\psi$ dihedral angle')
 plt.ylabel('Unitless FE')
 plt.legend()
-plt.title('EMUS and Iterative EMUS potentials of Mean Force')
-plt.show()
-
-# Plot the relative normalization constants as fxn of max iteration.
-plt.errorbar(np.arange(len(z)), -np.log(z),
-             yerr=np.sqrt(zerr)/z, label="Iteration 0")
-plt.plot(-np.log(z_iter_1), label="Iteration 1")
-plt.plot(-np.log(z_iter_1k), label="Iteration 1k", linestyle='--')
-plt.xlabel('Window Index')
-plt.ylabel('Unitless Free Energy')
-plt.title('Window Free Energies and Iter No.')
-plt.legend(loc='upper left')
+plt.title('Iterative EMUS potentials of Mean Force')
 plt.show()
 
 # Print the C7 ax basin probability
-print("EMUS Probability of C7ax basin is %f +/- %f" % (probC7ax, probC7ax_std))
-print("Iterative EMUS Probability of C7ax basin is %f" % (prob_C7ax_iter))
+print("Iterative EMUS Free Energy of C7ax basin is %f +/- %f" % (fe_C7ax, fe_C7ax_err))
 
-print("Asymptotic coefficient of variation for each partition function:")
-print(np.sqrt(zerr)/z)
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.semilogy(np.arange(len(z_iter)), fe_C7ax_contribs, c="C0")
+ax2.semilogy(np.arange(len(z_iter)), fe_C7ax_taus, c="C1")
+ax2.set_xlabel(r'Window Index')
+ax1.set_ylabel('Contribs to Error')
+ax2.set_ylabel('ACTimes')
+plt.show()
